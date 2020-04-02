772c8777e7f6b3b1c37789df49118ddcbd599771
David Foster <dlfosterii@yahoo.com>
2020-04-02 12:05:12 -0400

    complete exercise

--- n_to_m.py
+++ n_to_m.py
@@ -3,12 +3,12 @@
 
 #setup
 
-start_num = int(input(f'Enter a number to begin with: ' ))
-end_num = int(input(f'Enter a numbert to end with: '))
-
+start_num = int(input('Enter a number to begin with: ' ))
+end_num = int(input('Enter a numbert to end with: '))
+num = start_num
 
 #code to add and print numbers to console
-
-
-
+while num <= end_num:
+    print(num)
+    num += 1
 

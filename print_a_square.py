d702cb061281236b7ce10e0d9453f229d4500a06
David Foster <dlfosterii@yahoo.com>
2020-04-02 13:50:52 -0400

    completed exercise 9

---
+++ print_a_square.py
@@ -0,0 +1,18 @@
+#Print a 5x5 square of * characters
+
+#setup
+side = 5
+y = 0
+
+#Code to make it work
+
+while(y < side):
+    x = 0
+    while(x < side):      
+        x = x + 1
+        print('*', end = ' ')
+    y = y + 1
+    print('')
+
+
+#end

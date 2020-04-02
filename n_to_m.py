e521e94ca1bcea4ca5a3cd867ab5ad14d85f5597
David Foster <dlfosterii@yahoo.com>
2020-04-02 11:54:39 -0400

    start of project

---
+++ n_to_m.py
@@ -0,0 +1,14 @@
+#Same as the previous problem, except you will prompt the user for the number to 
+# start on and the number to end on. 
+
+#setup
+
+start_num = int(input(f'Enter a number to begin with: ' ))
+end_num = int(input(f'Enter a numbert to end with: '))
+
+
+#code to add and print numbers to console
+
+
+
+

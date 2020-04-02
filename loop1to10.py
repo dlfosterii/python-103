1d1b760e97078f4ff416f9ff3fd61c1dd675dc65
David Foster <dlfosterii@yahoo.com>
2020-04-02 11:45:35 -0400

    completed exercise

---
+++ loop1to10.py
@@ -0,0 +1,17 @@
+#Exercise:
+#Using a while loop, print out the numbers between 1 and 10 inclusive, one on a line.
+
+#Setup
+num = 1
+
+#process
+
+while num <= 10:
+    num += 1 #adding 1 to num
+    print(num)
+
+
+
+
+
+#Result
